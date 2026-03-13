073d9b7097c190021098a1d2aed841a9e262eceb
samuel filipe <samuelfilipe054@gmail.com>
2026-03-13 16:06:27 -0300

    added send invalid value test

--- cypress/e2e/sendMoney.cy.js
+++ cypress/e2e/sendMoney.cy.js
@@ -8,23 +8,45 @@ const loginPage = new LoginPage
 describe('Send Money Feature Test', () => {
 
    beforeEach(() => {
+      cy.request('POST', 'http://localhost:3001/testData/seed')
       loginPage.accessLoginPage()
       loginPage.loginWithUser(userData.userSuccess.username, userData.userSuccess.password)
+      
 })
 
-   it('Send Money Feature Test - Success', () => {
+   it.only('Send Money Feature Test - Success', () => {
       sendMoneyPage.getUserBalance().then((balanceBefore) => {
       const before = parseFloat(balanceBefore.replace('$', '').replace(',', ''))
           cy.intercept('POST', '/transactions').as('createTransaction')
-      sendMoneyPage.getUserBalance()
-      sendMoneyPage.sendMoneyValidValue(1, 'test')
+      
+      sendMoneyPage.sendMoney(1, 'valid value test')
+      sendMoneyPage.sendMoneyValidValue()
           cy.wait('@createTransaction')
+          cy.get('[data-test="sidenav-user-balance"]').should('not.contain', balanceBefore)
       sendMoneyPage.getUserBalance().then((balanceAfter) => {
       const after = parseFloat(balanceAfter.replace('$', '').replace(',', ''))
       expect(after).to.be.lessThan(before)
    })
   })
  })
+ 
+   it('Send Money With a Invalid Value - Fail', () => {
+      sendMoneyPage.getUserBalance().then((balanceBefore) => {
+      const before = parseFloat(balanceBefore.replace('$', '').replace(',', ''))
+          cy.intercept('POST', '/transactions').as('createTransaction')
+      sendMoneyPage.sendMoney(-1, 'invalid value test')
+      sendMoneyPage.sendMoneyInvalidValue()
+          cy.wait('@createTransaction')
+      sendMoneyPage.getUserBalance().then((balanceAfter) => {
+      const after = parseFloat(balanceAfter.replace('$', '').replace(',', ''))
+      expect(after).to.be.equal(before)
+      //BUG: System allows negative transaction values
+    })
+   })
+  })
+
+
+
 
 })
 
--- cypress/pages/sendMoneyPage.js
+++ cypress/pages/sendMoneyPage.js
@@ -11,24 +11,32 @@ class SendMoneyPage {
        checkPaymentSuccess: '[data-test="alert-bar-success"]',
        userBalance: '[data-test="sidenav-user-balance"]',
        returnButton: '[data-test="new-transaction-return-to-transactions"]',
+       createNewTransactionButton: '[data-test="new-transaction-create-another-transaction"]',
    }
 return selectors}
  
-    sendMoneyValidValue(amount, note) {
+    sendMoney(amount, note) {
      cy.get(this.selectorsList().newTransactionButton).click()
      cy.get(this.selectorsList().checkSeachBar).should('be.visible')
      cy.get(this.selectorsList().selectContactGrid).click()
      cy.get(this.selectorsList().addAmountField).type(amount)
      cy.get(this.selectorsList().addNoteField).type(note)
      cy.get(this.selectorsList().paymentButton).click()
-     cy.contains('test').should('be.visible')
-     cy.get(this.selectorsList().returnButton).click()
  }
 
     getUserBalance() {
      return cy.get(this.selectorsList().userBalance).invoke('text')
  }
 
+    sendMoneyValidValue() {
+     cy.contains('valid value test').should('be.visible')
+     cy.get(this.selectorsList().returnButton).click()
+ }
+
+    sendMoneyInvalidValue() {
+     cy.contains('invalid amount').should('be.visible')
+
+ }
 
 
 }
